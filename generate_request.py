1f8ee03f2201015bb1b873700a211821f05adfc5
John Dahle <dahlej@gmail.com>
2022-09-21 13:52:56 -0500

    First Deposit

--- generate_request.py
+++ generate_request.py
@@ -6,15 +6,15 @@ class GenerateRequest(BaseModel):
     image_id: str
     prompt: str
     initial_image_path: Union[str, None] = None
-    ddim_steps: float | Union[str, None] = 50
-    ddim_eta: float | Union[str, None] = 0.0
-    verbose: bool | Union[bool, None] = False
-    count: int | Union[int, None] = 1
-    scale: float | Union[float, None] = 5.0
-    channels: int | Union[int, None] = 4
-    width: int | Union[int, None] = 512
-    height: int | Union[int, None] = 512
-    down_sample_factor: int | Union[int, None] = 8
-    start_code: str | Union[str, None] = None
-    seed: int | Union[str, int] = 64
-    ckpt: str | Union[str, None] = "models/ldm/stable-diffusion-v1/model.ckpt"
+    ddim_steps: Union[float, None] = 50
+    ddim_eta: Union[float, None] = 0.0
+    verbose: Union[bool, None] = False
+    count: Union[int, None] = 1
+    scale: Union[float, None] = 5.0
+    channels: Union[int, None] = 4
+    width: Union[int, None] = 512
+    height: Union[int, None] = 512
+    down_sample_factor: Union[int, None] = 8
+    start_code: Union[str, None] = None
+    seed: Union[str, int] = 64
+    ckpt: Union[str, None] = "models/ldm/stable-diffusion-v1/model.ckpt"
